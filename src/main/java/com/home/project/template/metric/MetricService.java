1a014ed16c499a48c5c2c3f6959c6a26fd945643
maxdrv <maxdrv.stud@gmail.com>
2023-07-26 10:28:57 +0200

    monitor slru flag

--- src/main/java/com/home/project/template/metric/MetricService.java
+++ src/main/java/com/home/project/template/metric/MetricService.java
@@ -1,5 +1,6 @@
 package com.home.project.template.metric;
 
+import com.home.project.template.configuration.ConfigurationRepository;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Tag;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -7,17 +8,25 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 
 @Component
 public class MetricService {
+
+    private final ConfigurationRepository configurationRepository;
     private final MeterRegistry meterRegistry;
     private final MetricRepository metricRepository;
     private final Map<MetricIdentity, AtomicLong> metrics;
 
-    public MetricService(MeterRegistry meterRegistry, MetricRepository metricRepository) {
+    public MetricService(
+            ConfigurationRepository configurationRepository,
+            MeterRegistry meterRegistry,
+            MetricRepository metricRepository
+    ) {
+        this.configurationRepository = configurationRepository;
         this.meterRegistry = meterRegistry;
         this.metricRepository = metricRepository;
         this.metrics = new ConcurrentHashMap<>();
@@ -36,12 +45,18 @@ public class MetricService {
             Tag host = Tag.of("host_custom", identity.host());
             metrics.put(identity, meterRegistry.gauge(identity.name(), List.of(table, space, host), new AtomicLong(0)));
         }
-        for (Metric metric : metricRepository.slru()) {
-            MetricIdentity identity = metric.identity();
-            Tag slru = Tag.of("slru", identity.key());
-            Tag type = Tag.of("blks", identity.subKey());
-            Tag host = Tag.of("host_custom", identity.host());
-            metrics.put(identity, meterRegistry.gauge(identity.name(), List.of(slru, type, host), new AtomicLong(0)));
+
+        boolean monitorSlru = configurationRepository.findByKey("slru")
+                .map(conf -> Objects.equals("true", conf.value()))
+                .orElse(false);
+        if (monitorSlru) {
+            for (Metric metric : metricRepository.slru()) {
+                MetricIdentity identity = metric.identity();
+                Tag slru = Tag.of("slru", identity.key());
+                Tag type = Tag.of("blks", identity.subKey());
+                Tag host = Tag.of("host_custom", identity.host());
+                metrics.put(identity, meterRegistry.gauge(identity.name(), List.of(slru, type, host), new AtomicLong(0)));
+            }
         }
 
         for (String host : List.of("master", "replica")) {
@@ -65,9 +80,17 @@ public class MetricService {
         for (Metric metric : metricRepository.tableSpace()) {
             metrics.get(metric.identity()).set(metric.value());
         }
-        for (Metric metric : metricRepository.slru()) {
-            metrics.get(metric.identity()).set(metric.value());
+
+        boolean monitorSlru = configurationRepository.findByKey("slru")
+                .map(conf -> Objects.equals("true", conf.value()))
+                .orElse(false);
+
+        if (monitorSlru) {
+            for (Metric metric : metricRepository.slru()) {
+                metrics.get(metric.identity()).set(metric.value());
+            }
         }
+
         for (Metric metric : metricRepository.waitEvents()) {
             metrics.get(metric.identity()).set(metric.value());
         }
